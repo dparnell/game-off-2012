ea893087d9853a1d9ccf0f9776f8ecff3dd13ca0
Daniel Parnell <me@danielparnell.com>
2012-11-07 16:05:18 +1100

    Center the header text

--- js/app.js
+++ js/app.js
@@ -82,13 +82,6 @@ $(document).ready(function() {
 
     // create a point light
     var pointLight = new THREE.PointLight( 0xFFFFFF );
-
-    // set its position
-/*
-    pointLight.position.x = 10;
-    pointLight.position.y = 50;
-    pointLight.position.z = 130;
-*/
     pointLight.tick = function() { this.position = new THREE.Vector3(130*Math.sin(-th/10), 130*Math.cos(th), 130) }
     // add to the scene
     scene.add(pointLight);
