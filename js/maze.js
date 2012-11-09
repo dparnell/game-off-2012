89cf4eac7dc3d32e4fe421e4c0450ad2bebe8b43
Daniel Parnell <me@danielparnell.com>
2012-11-09 14:13:38 +1100

    Maze generation isn't quite working yet.  Need a better algorithm

--- js/maze.js
+++ js/maze.js
@@ -8,11 +8,15 @@
     var OFFSET_X_BY = 5;
     var OFFSET_Y_BY = 5;
 
+    var MAZE_SPEED = 10;
+
     var THREE = null;
     var cubes = [];
     var walls = [];
     var scene;
     var cubeMaterial;
+
+    var box;
 
     function clearMaze() {
 	// make sure there are no cubes left in the scene
@@ -20,6 +24,158 @@
 	    scene.remove(cube);
 	}
 	cubes = [];
+    }
+
+    function mazeBuilder() {
+	var x = 0;
+	var y = 0;
+	var direction = 0;
+	var distance = Math.floor(Math.random()*10)+1;
+	var todo = [];
+
+	function lookAhead(dir, dist) {
+	    if(dir==0) {
+		if(y+dist<MAZE_HEIGHT) {
+		    return cubes[x+(y+dist)*MAZE_WIDTH];
+		}
+	    }
+	    if(dir==1) {
+		if(x+dist<MAZE_WIDTH) {
+		    return cubes[(x+dist)+y*MAZE_WIDTH];
+		}
+	    }
+	    if(dir==2) {
+		if(y-dist>=0) {
+		    return cubes[x+(y-dist)*MAZE_WIDTH];
+		}
+	    }
+	    if(dir==3) {
+		if(x-dist>=0) {
+		    return cubes[(x-dist)+y*MAZE_WIDTH];
+		}
+	    }
+
+	    return false;
+	}
+	
+	function neighbourCount() {
+	    var result = 0;
+
+	    if(x>0) {
+		if(cubes[(x-1)+y*MAZE_WIDTH]) {
+		    result++;
+		}
+	    } else {
+		result++;
+	    }
+	    if(x<MAZE_WIDTH-1) {
+		if(cubes[(x+1)+y*MAZE_WIDTH]) {
+		    result++;
+		}
+	    } else {
+		result++;
+	    }
+	    if(y>0) {
+		if(cubes[x+(y-1)*MAZE_WIDTH]) {
+		    result++;
+		}
+	    } else {
+		result++;
+	    }
+	    if(y<MAZE_HEIGHT-1) {
+		if(cubes[x+(y+1)*MAZE_WIDTH]) {
+		    result++;
+		}
+	    } else {
+		result++;
+	    }
+	    return result;
+	}
+
+	var builder = function() {
+//	    console.info({'x':x,'y':y,'direction':direction,'distance':distance});
+
+	    if(neighbourCount()<2) {
+		distance = 0;
+	    }
+
+	    if(distance>0) {
+		var cube = cubes[x+y*MAZE_WIDTH];
+		scene.remove(cube);
+		cubes[x+y*MAZE_WIDTH] = null;
+
+		if(direction==0) {
+		    y = y + 1;
+		} else if(direction==1) {
+		    x = x + 1;
+		} else if(direction==2) {
+		    y = y - 1;
+		} else {
+		    x = x - 1;
+		}
+
+		if(x<0) {
+		    x = 0;
+		    distance = 0;
+		} else if(x==MAZE_WIDTH) {
+		    x = MAZE_WIDTH-1;
+		    distance = 0;
+		} else if(y<0) {
+		    y = 0;
+		    distance = 0;
+		} else if(y==MAZE_HEIGHT) {
+		    y = MAZE_HEIGHT-1;
+		    distance = 0;
+		} else {
+		    distance--;
+		    if(distance>0) {
+//			todo.push({'x': x, 'y': y});
+			global.setTimeout(builder, MAZE_SPEED);
+		    }
+		}
+	    }
+
+	    if(distance<=0) {
+		var new_dir = (direction + 1) & 3;
+		var found = false;
+		var counter = 4;
+		while(counter>0 && !found) {
+		    found = lookAhead(new_dir, 1) && lookAhead(new_dir, 2);
+
+		    if(!found) {
+			new_dir = (new_dir+1) & 3;
+		    }
+
+		    counter--;
+		}
+
+		if(found) {
+		    todo.push({'x': x, 'y': y});
+		    direction = new_dir;
+		    distance = Math.floor(Math.random()*10)+1;
+		    global.setTimeout(builder, MAZE_SPEED);
+		} else {
+  		    if(todo.length>0) {
+			var p = todo.pop();
+			x = p.x;
+			y = p.y;
+
+			console.info(p);
+			global.setTimeout(builder, MAZE_SPEED);
+		    } else {
+			console.error(todo);
+			scene.remove(box);
+		    }
+
+		}
+	    }
+
+	    box.position.x = (x-MAZE_WIDTH/2) * CUBE_SIZE * CUBE_FUDGE + OFFSET_X_BY;
+	    box.position.y = (y-MAZE_HEIGHT/2) * CUBE_SIZE * CUBE_FUDGE + OFFSET_Y_BY;
+	    box.position.z = 0;
+	}
+
+	return builder;
     }
 
     function Maze() {
@@ -41,7 +197,13 @@
 		scene.add(cube);
 	    }
 	}
+
+	scene.add(box);
+	
+	global.setTimeout(mazeBuilder(), MAZE_SPEED);
     }
+
+    Maze.prototype.clearMaze = clearMaze;
 
     global.init_maze = function(three, aScene) {
 	if(THREE==null) {
@@ -65,6 +227,12 @@
 	    walls[3] = new THREE.Mesh(new THREE.CubeGeometry(CUBE_SIZE, CUBE_SIZE * MAZE_HEIGHT * CUBE_FUDGE, CUBE_SIZE), cubeMaterial);
 	    walls[3].position = new THREE.Vector3(-(MAZE_WIDTH/2+1) * CUBE_SIZE * CUBE_FUDGE + OFFSET_X_BY, -OFFSET_Y_BY/2,  0);
 	    scene.add(walls[3]);
+
+	    box = new THREE.Mesh(
+		new THREE.CubeGeometry(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE),
+		new THREE.MeshLambertMaterial( { color: 0xcc6600 } )
+	    );
+
 	}
 
 	return new Maze();
